c69d283c74bd30a3cfae780c00d979adfa34e361
André Steenbergen <andre.steenbergen@gmail.com>
2020-07-31 17:12:54 +0200

    Moved Directory

---
+++ FrontEnd/src/constants/industries.js
@@ -0,0 +1,8 @@
+export const industries = {
+    COTTON : 'Cotton Mill',
+    MAN_GOODS : 'Manufactured Goods',
+    COAL : "Coal Mine",
+    POTTERY : "Pottery",
+    BEER : "Beer Brewery",
+    IRON : "Ironworks"
+}
