14e2fe4e25b5c2dcb33fe38fb4c0efeb7208b0f6
André Steenbergen <andre.steenbergen@gmail.com>
2020-08-04 23:50:30 +0200

    board toegevoegd

--- FrontEnd/src/components/CurrentGame.js
+++ FrontEnd/src/components/CurrentGame.js
@@ -6,7 +6,16 @@ import { Cities } from "../constants";
 class CurrentGame extends React.Component {
     render() {
         console.log( this.props );
-        return <span>CURRENT GAME: {this.props.gameId} ({this.props.numberOfPlayers} players)</span>
+        return <div>
+            <span>CURRENT GAME: {this.props.gameId} ({this.props.numberOfPlayers} players)</span>
+            <svg viewBox="0 0 4000 4000" style={{ background : 'url(/images/board.jpg)', backgroundSize : 'cover'}}>
+                {Object.keys(Cities).map(c => (
+                    <g key={c} transform={`translate(${Cities[c].coordinates.x} ${Cities[c].coordinates.y})`}>
+                        <text x="0" y="0" fontSize="22" style={{textAnchor: 'middle', dominantBaseline: 'middle'}}>{Cities[c].name}</text>
+                    </g>
+                ))}
+            </svg>
+        </div>
     }
 }
 
